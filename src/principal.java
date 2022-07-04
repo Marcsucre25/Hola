f1431abb73f5a46433195e7f3c6cc8a66b84d2b5
Marcsucre25 <105298870+Marcsucre25@users.noreply.github.com>
2022-07-04 09:47:31 -0500

    Initial commit

---
+++ src/principal.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class principal {
+    public static void main(String [] args) {
+        System.out.println("Hola a todos");
+        System.out.println("Marcos Moreira");
+
+    }
+}
